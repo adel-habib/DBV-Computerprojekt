04129f75cb7374452e534254b6a715f56f43be97
adel <adill.u@outlook.com>
2021-12-10 14:28:06 +0100

    Improved matching algorithm

--- match.m
+++ match.m
@@ -1,23 +1,36 @@
-function N_SQD = match(digit)
-%MATCH Summary of this function goes here
-%   Detailed explanation goes here
-    temp = imread("0.png");
+function [matched_array,euler,aspect_ration] = match(digit)
+%MATCH Matches digits to tempelates using normalised sum of differences
+% inputs:
+%   digit  : A binary image of single digit 
+% outputs:
+%   matched_array           : Array of confidence 
+%   euler                   : Euler number of digit
+%   aspect_ration           : Aspect ration of the digit
+
     
-    [width height] = size(temp);
-    n_digit = imresize(digit,[width height]);
-    sumj = 0;
-    sumi= 0;
-    sqd = 0;
-    for i=1:width
-        for j=1:height
-            
-            sqd = sqd + abs((temp(i,j)- n_digit(i,j)));
-            %sumj = sumj + temp(i,j);
-            %sumi = sumi + n_digit(i,j);
-            sqd_n = sqd / (width * height);
-            
-        end
+    matches = ones(10,1);
+    for im  = 1:10
+           sd               = 0; % sum of differences 
+           path             = num2str(im-1) + ".png";
+           tempelate        = imread(path);
+           [width, height]   = size(tempelate);
+           normalised_digit = imresize(digit,[width height]);
+           
+       for i= 1:width
+            for j= 1:height
+                
+                         sd = sd + abs((tempelate(i,j)- normalised_digit(i,j)));
+            end
+       end
+       
+       normalised_sd        = 1 - (sd / (width * height));
+       matches(im)          = normalised_sd;
+       
     end
-    N_SQD = sqd_n;
+    
+    aspect_ration           = size(digit,2)/size(digit,1);
+    euler                   = bweuler(digit);
+    matched_array           = matches;
+    
 end
 
--- showcase.m
+++ showcase.m
@@ -9,14 +9,16 @@ N_DIGITS = 6;
 
 % The weakness of this approach is that it's color senstive
 
-img = imread("tempelate_1.png");
+img = imread("im3.png");
 
 bw = segment(img);
 
 
 binaryImage = get_blobs(bw,N_DIGITS);
 for i=1:6
-   match(get_digit(binaryImage,i))
+    display(i)
+    [x,y,z] = match(get_digit(binaryImage,i))
+    fprintf("--------------- \n");
 end
 imshow(binaryImage)
 %bweuler(get_digit(binaryImage,3))
