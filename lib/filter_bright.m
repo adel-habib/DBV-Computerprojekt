c956fceaffc8efbcd588d2f6f50784a393a189ae
adel <adill.u@outlook.com>
2021-12-17 19:56:34 +0100

    Masking and filtering out noise / brightness

---
+++ lib/filter_bright.m
@@ -0,0 +1,25 @@
+function maskedRGBImage = filter_bright(RGB)
+%filter_bright Removes unwanted bright areas of an RGB Image 
+
+
+I = rgb2hsv(RGB);
+
+channel1Min = 0.241;
+channel1Max = 1.000;
+
+channel2Min = 0.721;
+channel2Max = 1.000;
+
+channel3Min = 0.721;
+channel3Max = 1.000;
+
+sliderBW = (I(:,:,1) >= channel1Min ) & (I(:,:,1) <= channel1Max) & ...
+    (I(:,:,2) >= channel2Min ) & (I(:,:,2) <= channel2Max) & ...
+    (I(:,:,3) >= channel3Min ) & (I(:,:,3) <= channel3Max);
+BW = sliderBW;
+
+maskedRGBImage = RGB;
+
+maskedRGBImage(repmat(~BW,[1 1 3])) = 0;
+
+end
