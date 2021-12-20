57d9baf0e34002fb7d138c2e6cea0f56434a5f52
adel <adill.u@outlook.com>
2021-12-20 21:40:18 +0100

    Performance testing

--- showcase.m
+++ showcase.m
@@ -1,7 +1,20 @@
+%% INCLUDE NECESSARY FOLDERS TO PATH
 addpath("images")
 addpath("images/tempelates")
 addpath("lib")
-path = "im3.png";
-im = imread(path);
-[conf, time] = read_display(path);
-imshow(im)
+
+%% PERFORMACE TEST OVER 10 different IMAGES
+NUM_IMAGES          = 10;
+NUM_DIGITS          = 6;
+images              = strings(1,NUM_IMAGES);
+t                   = ones(1,NUM_IMAGES);
+T                   = ones(1,NUM_DIGITS,NUM_IMAGES);
+C                   = ones(1,NUM_IMAGES);
+for i=1:10
+    images(i)       = "im" + num2str(i) + ".png";
+    tic
+    [C(i),T(:,:,i)] = read_display(images(i));
+    toc
+    t(i)            = toc;
+end
+sprintf("Mean time is %.3fs, Mean confidence is: %.2f%%",mean(t),mean(C)*100)
