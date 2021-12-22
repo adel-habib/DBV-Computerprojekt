a9ff72eda5a30179ab507295cb4c96d48fd5bcea
adel <adill.u@outlook.com>
2021-12-22 23:10:20 +0100

    Testing and showcasing

--- showcase.m
+++ showcase.m
@@ -1,9 +1,12 @@
+clc;
+clear;
+format long
 %% INCLUDE NECESSARY FOLDERS TO PATH
 addpath("images")
 addpath("images/tempelates")
 addpath("lib")
 
-%% PERFORMACE TEST OVER 10 different IMAGES
+%% PERFORMACE TEST OVER 10 different Images
 NUM_IMAGES          = 10;
 NUM_DIGITS          = 6;
 images              = strings(1,NUM_IMAGES);
@@ -18,3 +21,13 @@ for i=1:10
     t(i)            = toc;
 end
 sprintf("Mean time is %.3fs, Mean confidence is: %.2f%%",mean(t),mean(C)*100)
+
+%% Comparison of results with correct answers
+results             = ones(NUM_IMAGES,1);
+for i = 1: NUM_IMAGES
+    results(i)      = polyval(T(:,:,i),NUM_IMAGES);
+end
+TEST_DATA           = textread("correct.txt");
+table(TEST_DATA,results)
+
+
